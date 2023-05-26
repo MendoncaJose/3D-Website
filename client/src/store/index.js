f11efd29749fcf4ee58fe7eac13e1c15d2ffa1a9
MendoncaJose <mendoncajose7@gmail.com>
2023-05-26 13:11:02 +0100

    3D model finished

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,8 +5,8 @@ const state = proxy({
   color: '#EFBD48',
   isLogoTexture: true,
   isFullTexture: false,
-  logoDecal: './threejs.png',
-  fullDecal: './threejs.png',
+  logoDecal: './teta.png',
+  fullDecal: './teta.png',
 });
 
 export default state;
